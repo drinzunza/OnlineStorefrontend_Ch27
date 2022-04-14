a53c78c079f54d654bc6cc82ce77c8deffcbef38
Sergio Inzunza <inxunxa@gmail.com>
2022-04-14 16:58:48 -0600

    107 #3

---
+++ src/components/product.jsx
@@ -0,0 +1,35 @@
+import "./product.css";
+import QuantityPicker from "./quantityPicker";
+import { useState } from "react";
+
+const Product = (props) => {
+  const [quantity, setQuantity] = useState(1);
+
+  const handleQuantityChange = (val) => {
+    console.log("The quantity changed!", val);
+    setQuantity(val);
+  };
+
+  return (
+    <div className="product">
+      <img src={"/images/" + props.info.image}></img>
+
+      <h3>{props.info.title}</h3>
+
+      <div className="prices">
+        <label className="total">
+          <span>Total:</span>${(props.info.price * quantity).toFixed(2)}
+        </label>
+        <label className="price">
+          <span>Price:</span>${props.info.price.toFixed(2)}
+        </label>
+      </div>
+
+      <QuantityPicker onChange={handleQuantityChange}></QuantityPicker>
+
+      <button className="btn btn-sm btn-primary">Add to cart</button>
+    </div>
+  );
+};
+
+export default Product;
