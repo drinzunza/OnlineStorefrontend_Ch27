2c18380f2501a5d3da6e36a8dbebda6c07ac7b50
Sergio Inzunza <contact@sinzunza.com>
2022-04-28 17:26:55 -0600

    FSDI 107 #4

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Navbar from "./components/navBar";
 import Footer from "./components/footer";
 import Catalog from "./components/catalog";
+import Todo from "./components/todo";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
@@ -14,6 +15,9 @@ function App() {
       <Navbar></Navbar>
       <Catalog></Catalog>
       <About></About>
+
+      <Todo></Todo>
+
       <Footer></Footer>
     </div>
   );
